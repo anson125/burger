5a424865ec3396364324bd71f7d63113e339674f
Ken Tillman <kentillman@Kens-MacBook-Pro.local>
2018-04-08 18:05:24 -0400

    Burger Homework

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('BBQ Burger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Double Cheese Burger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Hamburger', false);
